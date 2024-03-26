e7d1fd86f3199adef21ad5e576dd80ebd96fc53f
APlonks <antoine.planque@gmail.com>
2024-03-26 23:11:43 +0000

    Deploying code-server using client-go

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,7 @@ import (
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -39,32 +40,71 @@ func main() {
 
 	deployment := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "demo-deployment",
+			Name: "code-server-deployment",
+			Labels: map[string]string{
+				"app": "code-server-deployment",
+			},
 		},
 		Spec: appsv1.DeploymentSpec{
-			Replicas: int32Ptr(2),
+			Replicas: int32Ptr(1),
 			Selector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{
-					"app": "demo",
+					"app": "code-server-pod",
 				},
 			},
 			Template: apiv1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
-						"app": "demo",
+						"app": "code-server-pod",
 					},
 				},
 				Spec: apiv1.PodSpec{
 					Containers: []apiv1.Container{
 						{
-							Name:  "web",
-							Image: "nginx:1.12",
+							Name:  "code-server-container",
+							Image: "docker.io/codercom/code-server:4.22.1-ubuntu",
+							Env: []apiv1.EnvVar{
+								{
+									Name:  "DOCKER_USER",
+									Value: "user",
+								},
+							},
 							Ports: []apiv1.ContainerPort{
 								{
-									Name:          "http",
+									Name:          "http-code",
 									Protocol:      apiv1.ProtocolTCP,
-									ContainerPort: 80,
+									ContainerPort: 8080,
 								},
+							},
+							Resources: apiv1.ResourceRequirements{
+								Limits: apiv1.ResourceList{
+									apiv1.ResourceMemory: resource.MustParse("256Mi"),
+									apiv1.ResourceCPU:    resource.MustParse("500m"),
+								},
+							},
+							VolumeMounts: []apiv1.VolumeMount{
+								{
+									Name:      "config-code-server",
+									MountPath: "/home/coder/.config",
+								},
+								{
+									Name:      "project-storage",
+									MountPath: "/home/coder/project",
+								},
+							},
+						},
+					},
+					Volumes: []apiv1.Volume{
+						{
+							Name: "config-code-server",
+							VolumeSource: apiv1.VolumeSource{
+								EmptyDir: &apiv1.EmptyDirVolumeSource{},
+							},
+						},
+						{
+							Name: "project-storage",
+							VolumeSource: apiv1.VolumeSource{
+								EmptyDir: &apiv1.EmptyDirVolumeSource{},
 							},
 						},
 					},
@@ -96,7 +136,7 @@ func main() {
 	prompt()
 	fmt.Println("Deleting deployment...")
 	deletePolicy := metav1.DeletePropagationForeground
-	if err := deploymentsClient.Delete(context.TODO(), "demo-deployment", metav1.DeleteOptions{
+	if err := deploymentsClient.Delete(context.TODO(), "code-server-deployment", metav1.DeleteOptions{
 		PropagationPolicy: &deletePolicy,
 	}); err != nil {
 		panic(err)
